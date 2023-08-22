ae722d21a1428502748a86e09501ea12817f6dc0
GeoSauer <geoffrey.sauer89@gmail.com>
2023-08-21 19:03:55 -0600

    adds state

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+
 import TodoList from "./components/TodoList";
 import NewTodo from "./components/NewTodo";
+import { Todo } from "./todo.model";
+
 const App: React.FC = () => {
-  const todos = [{ id: "t1", text: "Finish the course" }];
+  const [todos, setTodos] = useState<Todo[]>([]);
+  // const [todos, setTodos] = useState<{ id: string; text: string }[]>([]); //this was cleaned up with the todo.model.ts
+  // const todos = [{ id: "t1", text: "Finish the course" }];
 
   const todoAddHandler = (text: string) => {
-    console.log(text);
+    setTodos([{ id: Math.random().toString(), text: text }]);
   };
 
   return (
